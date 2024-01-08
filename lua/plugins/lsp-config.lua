09bb1f1bd2ac92c769a56d8e2de807280f7a1066
hy-fy <hollyalexisfogarty@gmail.com>
2024-01-07 23:03:08 -0500

    Add autopair completion\nAdded aesthetics to show git status\nAdded vim options from kickstart

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,42 +1,53 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "omnisharp",
-        "omnisharp-mono",
-        "gopls",
-      },
-    },
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {},
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.svelte.setup({})
-      lspconfig.gopls.setup({
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod" },
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"omnisharp",
+				"omnisharp-mono",
+				"gopls",
+			},
+			auto_install = true,
+		},
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = {},
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      --ensure_installed = { "c", "lua", "html", "javascript", "markdown" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {},
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"bash",
+				"go",
+				"python",
+				"lua",
+				"html",
+				"javascript",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"markdown",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
